efe48c21c61538f799ba75999dd0dbd4b5f7167c
Lucasholt124 <lucasholt2021@gmail.com>
2025-04-09 01:35:24 -0300

    corrigido

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,22 +1,24 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
+import { Typewriter } from "react-simple-typewriter";
 
 function Type() {
   return (
-    <Typewriter
-      data-aos="fade-down-right"
-      options={{
-        strings: [
+    <span data-aos="fade-down-right" style={{ fontWeight: "bold", fontSize: "1.5rem" }}>
+      <Typewriter
+        words={[
           "Desenvolvedor de software",
           "Freelancer",
-          " Desenvolvedor FullStack",
-          "Contribuidor de código aberto",
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
+          "Desenvolvedor FullStack",
+          "Contribuidor de código aberto"
+        ]}
+        loop={true}
+        cursor
+        cursorStyle="|"
+        typeSpeed={70}
+        deleteSpeed={50}
+        delaySpeed={1000}
+      />
+    </span>
   );
 }
 
